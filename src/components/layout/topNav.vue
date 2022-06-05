c9e4fa3f0b570622b4913f055813910b9b007934
Sunny <7960880+lammo@user.noreply.gitee.com>
2022-06-05 21:08:17 +0800

    头部顶级导航栏制作完毕

--- src/components/layout/topNav.vue
+++ src/components/layout/topNav.vue
@@ -1,6 +1,22 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref, VNode, VNodeRef } from "vue";
+
+// 创建ref列表存储a标签，并且又一个相应的函数设置ref
+const aRef: VNodeRef = ref([]);
+const setARef = (e: Event): VNodeRef => {
+  return aRef.value.push(e);
+};
+
+// 处理 Nav 被选中的class样式
+const handleSwitchNav = (e: MouseEvent) => {
+  e.preventDefault();
+  for (let i = 0; i < aRef.value.length; i++) {
+    aRef.value[i].className = "";
+  }
+  // 给target声明，不然爆红（target可能为null|undefined）
+  const target = e.target as HTMLElement;
+  target.className = "fst";
+};
 </script>
 
 <template>
@@ -9,33 +25,42 @@
       <h1 class="logo">
         <a href="/#"> 网易云音乐 </a>
       </h1>
-      <ul class="m-nav">
-        <li class="fst">
-          <a href="">发现音乐</a>
+      <ul @click="handleSwitchNav" class="m-nav">
+        <li>
+          <a :ref="setARef" class="fst" href="">发现音乐</a>
         </li>
         <li>
-          <a href="">我的音乐</a>
+          <a :ref="setARef" href="">我的音乐</a>
         </li>
         <li>
-          <a href=""> 关注 </a>
+          <a :ref="setARef" href=""> 关注 </a>
         </li>
         <li>
-          <a href=""> 商城 </a>
+          <a :ref="setARef" href=""> 商城 </a>
         </li>
         <li>
-          <a href=""> 音乐人 </a>
+          <a :ref="setARef" href=""> 音乐人 </a>
         </li>
         <li>
-          <a href="">下载客户端</a>
+          <a :ref="setARef" href="">下载客户端</a>
           <sup class="hot">&nbsp;</sup>
         </li>
       </ul>
       <div class="right">
         <div class="search">
-          <input class="search-input" type="text">
+          <label>
+            <input
+              class="search-input"
+              placeholder="音乐/视频/电台/用户"
+              type="text"
+            />
+            <span class="icon"></span>
+          </label>
         </div>
-        <a class="creater-center" href="">创作者中心</a>
-        <div class="top-login">登录</div>
+        <a class="creater-center" href=""> 创作者中心 </a>
+        <div class="top-login">
+          <a href="">登录</a>
+        </div>
       </div>
     </div>
   </div>
@@ -50,7 +75,7 @@
     width: 1100px;
     margin: 0 auto;
     display: flex;
-   align-items: center;
+    align-items: center;
     .logo {
       margin: 0;
       padding: 0;
@@ -96,17 +121,70 @@
           background: url("../../assets/topbar.png");
           background-position: -190px 0;
         }
-        &.fst {
-          a {
-            background: #000;
-            color: #fff;
-          }
+        a.fst {
+          background: #000;
+          color: #fff;
         }
       }
     }
-    .right{
+    .right {
+      width: 300px;
       margin-left: 60px;
       display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .creater-center {
+        width: 90px;
+        height: 32px;
+        box-sizing: border-box;
+        padding-left: 16px;
+        border: 1px solid #4f4f4f;
+        background-position: 0 -140px;
+        line-height: 33px;
+        color: #ccc;
+        border-radius: 20px;
+        &:hover {
+          color: #fff;
+          text-decoration: none;
+          border: 1px solid #ccc;
+        }
+      }
+      .top-login {
+        background-color: #242424;
+        border-bottom: 1px solid #000;
+        a {
+          color: #999;
+          width: 28px;
+          &:hover {
+            color: #787878;
+          }
+        }
+      }
+      .search {
+        position: relative;
+        .search-input {
+          width: 95%;
+          margin: 0;
+          padding: 15px 0;
+          color: #333;
+          height: 30px;
+          line-height: 16px;
+          text-indent: 28px;
+          border-radius: 32px;
+          outline: none;
+          border: none;
+        }
+        .icon {
+          position: absolute;
+          top: 0px;
+          left: 0px;
+          width: 30px;
+          height: 30px;
+          border-radius: 32px;
+          background: url(/src/assets/topbar.png);
+          background-position: 0 -99px;
+        }
+      }
     }
   }
 }
